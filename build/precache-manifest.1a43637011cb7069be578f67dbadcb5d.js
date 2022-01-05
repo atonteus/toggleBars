492d8c2c53c0c7cbef21bbb6a8398d761dbeadf5
Antonio Mateus <uidesign@fractalbases.com>
2022-01-04 23:53:42 -0300

    first commit

---
+++ build/precache-manifest.1a43637011cb7069be578f67dbadcb5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53964cb46deb6a0b5c2e09318d05d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0abad6f5dc763a880b26",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "0e92fef702a9f8ef746f",
+    "url": "/static/js/2.e1d9d235.chunk.js"
+  },
+  {
+    "revision": "0abad6f5dc763a880b26",
+    "url": "/static/js/main.7cc12ca1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
